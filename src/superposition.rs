bf3323da61d5bf0492fa40e9f21dd6549c68708e
Henrik Böving <hargonix@gmail.com>
2025-06-22 11:37:09 +0200

    feat: equality factoring

--- src/superposition.rs
+++ src/superposition.rs
@@ -3,11 +3,11 @@ use std::cmp::Ordering;
 use log::info;
 
 use crate::{
-    clause::{Clause, ClauseSet},
+    clause::{Clause, ClauseSet, Literal},
     clause_queue::ClauseQueue,
-    kbo::literal_kbo,
+    kbo::{literal_kbo, term_kbo},
     pretty_print::pretty_print,
-    subst::Substitutable,
+    subst::{Substitutable, Substitution},
     term_bank::TermBank,
 };
 
@@ -24,30 +24,51 @@ pub enum SuperpositionResult {
     StatementFalse,
 }
 
+/*
+Takes a `clause` and an index `check_lit_idx` to some literal `check_lit` in `clause` together
+with a substitution `subst`. Then checks whether `subst(check_lit)` is maximal in `subst(clause)`.
+
+Returns `None` if maximality check fails, otherwise `Some(subst(clause) \ subst(check_lit))`
+*/
+fn maximality_check(
+    clause: &Clause,
+    check_lit_idx: usize,
+    subst: &Substitution,
+    term_bank: &TermBank,
+) -> Option<Vec<Literal>> {
+    let mut new_literals = Vec::with_capacity(clause.len());
+    let check_lit = clause.get_literal(check_lit_idx).clone().subst_with(&subst, term_bank);
+    let ok = (0..clause.len())
+        .filter(|other_lit_idx| check_lit_idx != *other_lit_idx)
+        .all(|other_lit_idx| {
+            let other_lit = clause
+                .get_literal(other_lit_idx)
+                .clone()
+                .subst_with(&subst, term_bank);
+            if literal_kbo(&check_lit, &other_lit, term_bank) != Some(Ordering::Greater) {
+                new_literals.push(other_lit);
+                true
+            } else {
+                // abort: we are not maximal
+                false
+            }
+        });
+    if ok {
+        Some(new_literals)
+    } else {
+        None
+    }
+}
+
 fn equality_resolution(clause: &Clause, acc: &mut Vec<Clause>, term_bank: &TermBank) {
     info!("ERes working clause: {}", pretty_print(clause, term_bank));
     for literal_idx in 0..clause.len() {
         let literal = clause.get_literal(literal_idx);
         if literal.is_negative() {
             if let Some(subst) = literal.get_lhs().unify(literal.get_rhs(), term_bank) {
-                let mut new_literals = Vec::with_capacity(clause.len());
-                let new_lit = literal.clone().subst_with(&subst, term_bank);
-                let larger_idx = (0..clause.len())
-                    .filter(|new_literal_idx| literal_idx != *new_literal_idx)
-                    .find(|new_literal_idx| {
-                        let check_lit = clause
-                            .get_literal(*new_literal_idx)
-                            .clone()
-                            .subst_with(&subst, term_bank);
-                        if literal_kbo(&check_lit, &new_lit, term_bank) != Some(Ordering::Greater) {
-                            new_literals.push(check_lit);
-                            false
-                        } else {
-                            // abort: we are not maximal
-                            true
-                        }
-                    });
-                if larger_idx.is_none() {
+                if let Some(new_literals) =
+                    maximality_check(clause, literal_idx, &subst, term_bank)
+                {
                     let new_clause = Clause::new(new_literals);
                     info!(
                         "ERes derived clause: {}",
@@ -61,7 +82,55 @@ fn equality_resolution(clause: &Clause, acc: &mut Vec<Clause>, term_bank: &TermB
 }
 
 fn equality_factoring(clause: &Clause, acc: &mut Vec<Clause>, term_bank: &TermBank) {
-    //todo!()
+    info!("EFact working clause: {}", pretty_print(clause, term_bank));
+    for literal1_idx in 0..clause.len() {
+        let lit1 = clause.get_literal(literal1_idx);
+        if lit1.is_negative() {
+            continue;
+        }
+        let lit1_terms = [lit1.get_lhs(), lit1.get_rhs()];
+
+        for literal2_idx in 0..clause.len() {
+            let lit2 = clause.get_literal(literal2_idx);
+            if lit2.is_negative() || literal2_idx == literal1_idx {
+                continue;
+            }
+            let lit2_terms = [lit2.get_lhs(), lit2.get_rhs()];
+            for t1_idx in 0..2 {
+                for t2_idx in 0..2 {
+                    let l1_lhs = lit1_terms[t1_idx];
+                    let l1_rhs = lit1_terms[1 - t1_idx];
+                    let l2_lhs = lit2_terms[t2_idx];
+                    let l2_rhs = lit2_terms[1 - t2_idx];
+
+                    if let Some(subst) = l1_lhs.unify(l2_lhs, term_bank) {
+                        if term_kbo(
+                            &l1_lhs.clone().subst_with(&subst, term_bank),
+                            &l1_rhs.clone().subst_with(&subst, term_bank),
+                            term_bank,
+                        ) != Some(Ordering::Less)
+                        {
+                            if let Some(mut new_literals) =
+                                maximality_check(clause, literal1_idx, &subst, term_bank)
+                            {
+                                let final_lit = Literal::mk_ne(
+                                    l1_rhs.clone().subst_with(&subst, term_bank),
+                                    l2_rhs.clone().subst_with(&subst, term_bank),
+                                );
+                                new_literals.push(final_lit);
+                                let new_clause = Clause::new(new_literals);
+                                info!(
+                                    "EFact derived clause: {}",
+                                    pretty_print(&new_clause, term_bank)
+                                );
+                                acc.push(new_clause);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 impl<'a> SuperpositonState<'a> {
