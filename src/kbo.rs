fbf585483045509a04c5cde22d48f941ee08b9ff
Daniel Soukup <d.soukup@campus.lmu.de>
2025-07-08 13:34:11 +0200

    feat: linear kbo transcript with less recursion

--- src/kbo.rs
+++ src/kbo.rs
@@ -1,6 +1,7 @@
 //! ## Knuth Bendix Order
-//! This module contains the naive implementation of Knuth Bendix Order (KBO) from
+//! This module contains an imperative adaption of the efficient implementation of Knuth Bendix Order (KBO) from
 //! ["Things to Know When Implementing KBO"](https://link.springer.com/content/pdf/10.1007/s10817-006-9031-4.pdf).
+//! Most notably we don't rely on a fixed size array to store the variable balance, but instead rely on a HashMap.
 //! It is additionally informed by ["E – A Brainiac Theorem Prover"](https://wwwlehre.dhbw-stuttgart.de/~sschulz/PAPERS/Schulz-AICOM-2002.pdf),
 //! in particular we draw:
 //! - `mu = 1`
@@ -10,53 +11,49 @@
 //!      in the term bank.
 //!
 //! The key type exposed is [KboOrd] together with its implementation for terms and literals.
+//!
+//! The general idea is that we traverse the two terms only once, but maintain a balance counter
+//! for the weight and each of the met variables.
+//!
+//! Mapping function names to paper names:
+//! Paper      | Code
+//! -----------|--------------
+//! inc        | inc_var
+//! dec        | dec_var
+//! tckbolex   | kbo_lex
+//! tckbo      | kbo_aux
+//! tckbo'     | kbo_functions
+//! mfyVWBc    | modify_balances_contains
+//! mfyVWBc_tl | modify_balances_list_contains
+//! mfyVWB_tl  | modify_balances_list
 
-use std::{cmp::Ordering, collections::HashMap};
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, hash_map::Entry},
+};
 
 use crate::{
     clause::{Literal, Polarity},
-    term_bank::{FunctionIdentifier, RawTerm, Term, TermBank},
+    position::LiteralSide,
+    term_bank::{FunctionIdentifier, RawTerm, Term, TermBank, VariableIdentifier},
 };
 
-// TODO: implement the optimized version.
+// TODO: evaluate kbo_6 pacman lemma
 
-const MU: usize = 1;
-
-fn phi(term: &Term) -> usize {
-    match &**term {
-        RawTerm::Var { .. } => MU,
-        RawTerm::App { args, .. } => MU + args.iter().map(phi).sum::<usize>(),
-    }
-}
+// Since the weight is calculated on the fly, there is no explicit phi(t):
+// - each variable and function symbol weights MU
+// - each function weights MU+phi(args)
+const MU: i32 = 1;
 
 struct KboComparator<'a> {
     term_bank: &'a TermBank,
+    var_balance: HashMap<VariableIdentifier, i32>,
+    weight_balance: i32,
+    num_neg: usize,
+    num_pos: usize,
 }
 
 impl<'a> KboComparator<'a> {
-    fn xlen(&self, term: &Term, i: usize) -> usize {
-        match &**term {
-            RawTerm::Var { id, .. } => {
-                if self.term_bank.get_variable_index(*id) as usize == i {
-                    1
-                } else {
-                    0
-                }
-            }
-            RawTerm::App { args, .. } => args.iter().map(|arg| self.xlen(arg, i)).sum::<usize>(),
-        }
-    }
-
-    fn var_check(&self, lhs: &Term, rhs: &Term) -> bool {
-        let k = self.term_bank.get_variable_count();
-        for i in 0..k {
-            if !(self.xlen(lhs, i) >= self.xlen(rhs, i)) {
-                return false;
-            }
-        }
-        true
-    }
-
     // Return true iff lhs has precedence over rhs
     fn function_precedence(&self, lhs: FunctionIdentifier, rhs: FunctionIdentifier) -> bool {
         let lhs_info = self.term_bank.get_function_info(lhs);
@@ -70,18 +67,178 @@ impl<'a> KboComparator<'a> {
         }
     }
 
-    fn kbolex(&self, ss: &[Term], ts: &[Term]) -> bool {
-        debug_assert_eq!(ss.len(), ts.len());
-        for (s, t) in ss.iter().zip(ts.iter()) {
-            if s != t {
-                return self.kbo_aux(s, t);
+    fn inc_var(&mut self, id: VariableIdentifier) {
+        match self.var_balance.entry(id) {
+            Entry::Occupied(mut entry) => {
+                let num = *entry.get();
+                if num == 0 {
+                    self.num_pos += 1;
+                } else if num == -1 {
+                    self.num_neg -= 1;
+                }
+                entry.insert(num + 1);
+            }
+            Entry::Vacant(entry) => {
+                self.num_pos += 1;
+                entry.insert(1);
+            }
+        }
+    }
+
+    fn dec_var(&mut self, id: VariableIdentifier) {
+        match self.var_balance.entry(id) {
+            Entry::Occupied(mut entry) => {
+                let num = *entry.get();
+                if num == 0 {
+                    self.num_neg += 1;
+                } else if num == 1 {
+                    self.num_pos -= 1;
+                }
+                entry.insert(num - 1);
+            }
+            Entry::Vacant(entry) => {
+                self.num_neg += 1;
+                entry.insert(-1);
+            }
+        }
+    }
+
+    fn modify_balances_list(&mut self, ss: &[Term], pos: LiteralSide) {
+        for s in ss.iter() {
+            match &**s {
+                RawTerm::Var { id: x, .. } => match pos {
+                    LiteralSide::Left => {
+                        self.inc_var(*x);
+                        self.weight_balance += MU;
+                    }
+                    LiteralSide::Right => {
+                        self.dec_var(*x);
+                        self.weight_balance -= MU;
+                    }
+                },
+                RawTerm::App { args: ss, .. } => {
+                    self.modify_balances_list(ss, pos);
+                    match pos {
+                        LiteralSide::Left => self.weight_balance += MU,
+                        LiteralSide::Right => self.weight_balance -= MU,
+                    }
+                }
+            }
+        }
+    }
+
+    fn modify_balances_list_contains(
+        &mut self,
+        ss: &[Term],
+        y: &VariableIdentifier,
+        pos: LiteralSide,
+    ) -> bool {
+        for (idx, s) in ss.iter().enumerate() {
+            if self.modify_balances_contains(s, y, pos) {
+                // Simply modify the balances for the remaining terms
+                // since y already occurs in s
+                self.modify_balances_list(&ss[idx + 1..], pos);
+                return true;
             }
         }
         false
     }
 
-    fn kbo_aux(&self, lhs: &Term, rhs: &Term) -> bool {
+    fn modify_balances_contains(
+        &mut self,
+        t: &Term,
+        y: &VariableIdentifier,
+        pos: LiteralSide,
+    ) -> bool {
+        match &**t {
+            RawTerm::Var { id: x, .. } => match pos {
+                LiteralSide::Left => {
+                    self.inc_var(*x);
+                    self.weight_balance += MU;
+                    x == y
+                }
+                LiteralSide::Right => {
+                    self.dec_var(*x);
+                    self.weight_balance -= MU;
+                    x == y
+                }
+            },
+            RawTerm::App { args: ss, .. } => {
+                let result = self.modify_balances_list_contains(ss, y, pos);
+                match pos {
+                    LiteralSide::Left => self.weight_balance += MU,
+                    LiteralSide::Right => self.weight_balance -= MU,
+                }
+                result
+            }
+        }
+    }
+
+    // Only called if f = g, which implies identical arity
+    fn kbo_lex(&mut self, ss: &[Term], ts: &[Term]) -> Option<Ordering> {
+        debug_assert_eq!(ss.len(), ts.len());
+        for (idx, (s, t)) in ss.iter().zip(ts.iter()).enumerate() {
+            let result = self.kbo_aux(s, t);
+            // Lexicographic iteration: skip until we find unequal terms
+            if let Some(Ordering::Equal) = result {
+                continue;
+            } else {
+                // Fix balances of the rest, then return
+                self.modify_balances_list(&ss[idx + 1..], LiteralSide::Left);
+                self.modify_balances_list(&ts[idx + 1..], LiteralSide::Right);
+                return result;
+            }
+        }
+        Some(Ordering::Equal)
+    }
+
+    fn kbo_functions(
+        &mut self,
+        f: &FunctionIdentifier,
+        g: &FunctionIdentifier,
+        ss: &[Term],
+        ts: &[Term],
+    ) -> Option<Ordering> {
+        // Weight cancels out, so we only have to focus on the args
+        if f == g {
+            self.kbo_lex(ss, ts)
+        } else {
+            self.modify_balances_list(ss, LiteralSide::Left);
+            self.modify_balances_list(ts, LiteralSide::Right);
+            None
+        }
+    }
+
+    fn kbo_aux(&mut self, lhs: &Term, rhs: &Term) -> Option<Ordering> {
         match (&**lhs, &**rhs) {
+            (RawTerm::Var { id: x, .. }, RawTerm::Var { id: y, .. }) => {
+                // Weight cancels out, so we only have to modify the variable balance
+                self.inc_var(*x);
+                self.dec_var(*y);
+                if x == y { Some(Ordering::Equal) } else { None }
+            }
+            (RawTerm::Var { id: x, .. }, RawTerm::App { .. }) => {
+                let contained = self.modify_balances_contains(&*rhs, &*x, LiteralSide::Right);
+                // Update variable and weight balance only for the variable part
+                self.inc_var(*x);
+                self.weight_balance = self.weight_balance + MU;
+                if contained {
+                    Some(Ordering::Less)
+                } else {
+                    None
+                }
+            }
+            (RawTerm::App { .. }, RawTerm::Var { id: y, .. }) => {
+                let contained = self.modify_balances_contains(&*lhs, &*y, LiteralSide::Left);
+                // Update variable and weight balance only for the variable part
+                self.dec_var(*y);
+                self.weight_balance = self.weight_balance - MU;
+                if contained {
+                    Some(Ordering::Greater)
+                } else {
+                    None
+                }
+            }
             (
                 RawTerm::App {
                     id: f, args: ss, ..
@@ -90,27 +247,49 @@ impl<'a> KboComparator<'a> {
                     id: g, args: ts, ..
                 },
             ) => {
-                if self.var_check(lhs, rhs) {
-                    let lphi = phi(lhs);
-                    let rphi = phi(rhs);
-                    match lphi.cmp(&rphi) {
-                        Ordering::Greater => true,
-                        Ordering::Equal => {
-                            self.function_precedence(*f, *g) || (f == g && self.kbolex(ss, ts))
-                        }
-                        Ordering::Less => false,
-                    }
+                // We assign each function symbol the same weight, so they cancel out
+                let lex_ord = self.kbo_functions(f, g, ss, ts);
+                let g_or_n = if self.num_neg == 0 {
+                    Some(Ordering::Greater)
                 } else {
-                    false
+                    None
+                };
+                let l_or_n = if self.num_pos == 0 {
+                    Some(Ordering::Less)
+                } else {
+                    None
+                };
+                if self.weight_balance > 0 {
+                    g_or_n
+                } else if self.weight_balance < 0 {
+                    l_or_n
+                } else if self.function_precedence(*f, *g) {
+                    g_or_n
+                } else if self.function_precedence(*g, *f) {
+                    l_or_n
+                } else if f != g {
+                    None
+                } else if lex_ord == Some(Ordering::Equal) {
+                    Some(Ordering::Equal)
+                } else if lex_ord == Some(Ordering::Greater) {
+                    g_or_n
+                } else if lex_ord == Some(Ordering::Less) {
+                    l_or_n
+                } else {
+                    None
                 }
             }
-            (RawTerm::App { .. }, RawTerm::Var { id: var_id, .. }) => var_id.occurs_in(lhs),
-            (RawTerm::Var { .. }, _) => false,
         }
     }
 
-    fn kbo(lhs: &Term, rhs: &Term, term_bank: &'a TermBank) -> bool {
-        let cmp = Self { term_bank };
+    fn kbo(lhs: &Term, rhs: &Term, term_bank: &'a TermBank) -> Option<Ordering> {
+        let mut cmp = Self {
+            term_bank: term_bank,
+            var_balance: HashMap::new(),
+            weight_balance: 0,
+            num_neg: 0,
+            num_pos: 0,
+        };
         cmp.kbo_aux(lhs, rhs)
     }
 }
@@ -123,12 +302,8 @@ impl KboOrd for Term {
     fn kbo(&self, other: &Self, term_bank: &TermBank) -> Option<Ordering> {
         if self == other {
             Some(Ordering::Equal)
-        } else if KboComparator::kbo(self, other, term_bank) {
-            Some(Ordering::Greater)
-        } else if KboComparator::kbo(other, self, term_bank) {
-            Some(Ordering::Less)
         } else {
-            None
+            KboComparator::kbo(self, other, term_bank)
         }
     }
 }
