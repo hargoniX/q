d049b59c22c1dc2d2acbe5c17a1783995772f5d0
Daniel Soukup <d.soukup@campus.lmu.de>
2025-07-18 19:04:39 +0200

    chore: new measurements

--- benchmark/plotter.sh
+++ benchmark/plotter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
-python plotter.py --mode time --duration 3 --variant pelletier
+python plotter.py --mode time --duration 10 --variant pelletier
 python plotter.py --mode time --duration 10 --variant casc24
 python plotter.py --mode time --duration 10 --variant casc29
