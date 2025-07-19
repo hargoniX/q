d441361a3d6bfe5bc48d5db621688ec756c47a33
Daniel Soukup <d.soukup@campus.lmu.de>
2025-07-19 15:21:51 +0200

    test: all fof theorems -Arithmetic

--- benchmark/runner.sh
+++ benchmark/runner.sh
@@ -8,6 +8,8 @@ if [ "${variant}" == "pelletier" ]; then
   python benchmark/runner.py -d $duration -m pelletier -f benchmark/pelletier.toml
 elif [ "${variant}" == "theorems-easy" ]; then
   python benchmark/runner.py -d $duration -m pelletier -f benchmark/theorems-easy.toml
+elif [ "${variant}" == "theorems" ]; then
+  python benchmark/runner.py -d $duration -m pelletier -f benchmark/theorems.toml
 elif [ "${variant}" == "casc29" ]; then
   python benchmark/runner.py -d $duration -m casc29 -c fof
   python benchmark/runner.py -d $duration -m casc29 -c fnt
@@ -15,6 +17,6 @@ elif [ "${variant}" == "casc24" ]; then
   python benchmark/runner.py -d $duration -m casc24 -c fof
   python benchmark/runner.py -d $duration -m casc24 -c fnt
 else
-  echo "${variant} has no controlflow! Existing Variants: 'pelletier', 'casc29', 'casc24', 'theorems-easy'"
+  echo "${variant} has no controlflow! Existing Variants: 'pelletier', 'casc29', 'casc24', 'theorems-easy', 'theorems'"
 fi
 popd > /dev/null
