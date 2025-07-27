36e27c748e73add79f1fc6d58f3a1f749be9c0a5
Daniel Soukup <d.soukup@campus.lmu.de>
2025-07-27 09:57:25 +0200

    new plots for t=30

--- benchmark/plotter.sh
+++ benchmark/plotter.sh
@@ -3,10 +3,10 @@ set -xe
 REPO_ROOT=`git rev-parse --show-toplevel`
 
 pushd $REPO_ROOT"/benchmark" > /dev/null
-python plotter.py --mode time --duration 10 --variant pelletier -f pelletier.toml
-python plotter.py --mode time --duration 10 --variant casc24
+python plotter.py --mode time --duration 30 --variant pelletier -f pelletier.toml
+python plotter.py --mode time --duration 30 --variant casc24
 python plotter.py --mode time --duration 10 --variant casc29
-python plotter.py --mode time --duration 1 --variant pelletier -f theorems.toml
+python plotter.py --mode time --duration 30 --variant pelletier -f theorems.toml
 python plotter.py --mode time --duration 1 --variant pelletier -f contradictory.toml
 python plotter.py --mode time --duration 1 --variant pelletier -f counter_satisfiable.toml
 python plotter.py --mode time --duration 1 --variant pelletier -f satisfiable.toml
